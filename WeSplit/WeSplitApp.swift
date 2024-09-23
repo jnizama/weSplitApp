48b9e3c3ff939f4c7a50d384e0254f44cd125fcd
Jorge <jorgenizamarios@gmail.com>
2024-09-23 03:35:31 +0200

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Jorge Nizama on 23/09/24.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
